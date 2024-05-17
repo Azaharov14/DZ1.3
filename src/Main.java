e8f82a84aeb0153a2a8a096002db8e943a2c228d
origin <git@github.com:Azaharov14/helloworld.git>
2024-05-17 09:31:08 +0300

    two commit

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
 
         int tempa = 20;
-        if (tempa <= 5) {
+        if (tempa < 5) {
             System.out.println("На улице " + tempa + " градусов , нужно надеть шапку");
         } else {
             System.out.println("На улице " + tempa + " градусов , можно идти без шапки");
@@ -59,7 +59,7 @@ public class Main {
         if (babyEars < 5) {
             System.out.println("Если возраст ребенка равен " + babyEars + " , то ему нельзя кататься на аттракционе.");
         } else {
-            if (babyEars > 5 && babyEars <= 14) {
+            if (babyEars >= 5 && babyEars <= 14) {
                 System.out.println("Если возраст ребенка равен " + babyEars + " , то ему можно кататься на аттракционе в сопровождении");
             } else {
                 if (babyEars > 14) {
@@ -86,16 +86,17 @@ public class Main {
         }
 
 
-        int one = 1;
-        int two = 2;
-        int three = 3;
-        if (one > two && one > three) {
+        int one = 10;
+        int two = 22;
+        int three = 13;
+        boolean greatsNumber = one > two && one > three;
+        if (greatsNumber) {
             System.out.println(one);
-        }
-        if (two > one && two > three) {
+        } else if (two > one && two > three) {
             System.out.println(two);
-        } else
+        } else if (one < three && two < three) {
             System.out.println(three);
+        }
     }
 }
 
